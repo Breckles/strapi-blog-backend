89faa8c13ade272e238f46de5e653a8996b1871d
Waylon <wayloncn@gmail.com>
2021-03-23 15:00:46 -0400

    initial setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5169852a9e66f99feba7a6131e360653'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5864d7df-7303-46aa-9619-f61404b9e0b3'
+};
